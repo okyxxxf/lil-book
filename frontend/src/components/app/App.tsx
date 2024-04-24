81eab9c693120c124081b537cd4de87a53b2a3a2
alexander <okyxxxf@gmail.com>
2024-04-24 21:43:10 +0300

    feat: add react router and create needed routes

--- frontend/src/components/app/App.tsx
+++ frontend/src/components/app/App.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export function App() {
   return (
-    <></>
+    <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<>layout</>}>
+        <Route path="/" element={<>Auth</>}/>
+        <Route path="/admin" element={<>latoyt admin</>}>
+          <Route path="/authors" element={<>Авторы</>}/>
+          <Route path="/books" element={<>Books</>}/>
+          <Route path="/cities" element={<>City</>}/>
+          <Route path="/issuings" element={<>Issuing</>}/>
+          <Route path="/library-cards" element={<>Lib cards</>}/>
+          <Route path="/publishers" element={<>Publisher</>}/>
+          <Route path="/reader" element={<>Reader</>}/>
+        </Route>
+      </Route>
+     </Routes>
+    </BrowserRouter>
   );
 }
