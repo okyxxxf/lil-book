d073bda7e1c5a7dd716937f1e10dc96ad19adbd4
alexander <okyxxxf@gmail.com>
2024-04-24 23:23:14 +0300

    feat: add leave button to aside

--- frontend/src/components/aside/Aside.tsx
+++ frontend/src/components/aside/Aside.tsx
@@ -1,6 +1,10 @@
 import { Box, Flex, Image, Link, Text, VStack } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import { CiExport } from "react-icons/ci";
 
 export function Aside() {
+  const navigator = useNavigate();
+
   const links = [
     {name: "Бронирование", href: "/booking"},
     {name: "Выдачи", href: "/issuings"},
@@ -10,19 +14,33 @@ export function Aside() {
     {name: "Авторы", href: "/authors"},
     {name: "Издательства", href: "/publishers"},
     {name: "Города", href: "/cities"}
-  ]
+  ];
+
+  const handleLeave = () => {
+    localStorage.removeItem("isAuth");
+    navigator("/");
+  }
+
   return (
     <Box as="aside" minH="calc(100vh - 88px)" w="23%" bg="red.400" position="relative">
-      <VStack align="stretch" position="absolute" right="10px" top="40px">
-        {links.map(({name, href}) => (
-          <Link href={`/admin${href}`} _hover={{}}>
-            <Flex align="center" gap="20px" borderRadius="20px" _hover={{bg: "red.500"}} transition="0.4s" p="10px 50px">
-              <Image src={`${process.env.PUBLIC_URL}/images${href}.png`} alt={name}/>
-              <Text color="white">{name}</Text>
-            </Flex>
-          </Link>
-        ))}
-      </VStack>
+      <Flex direction="column" justify="space-between" align="end" p="40px 10px 25px 10px" h="100%">
+        <VStack align="stretch">
+          {links.map(({name, href}, i) => (
+            <Link href={`/admin${href}`} _hover={{}} key={i}>
+              <Flex align="center" gap="20px" borderRadius="20px" _hover={{bg: "red.500"}} transition="0.4s" p="10px 50px">
+                <Image src={`${process.env.PUBLIC_URL}/images${href}.png`} alt={name}/>
+                <Text color="white">{name}</Text>
+              </Flex>
+            </Link>
+          ))}
+        </VStack>
+        <Flex gap="10px" align="center" p="10px 50px" marginRight="40px" cursor="pointer" onClick={handleLeave}>
+          <Box transform="rotate(90deg)">
+            <CiExport color="white" size="24px"/>
+          </Box>
+          <Text color="white" fontWeight="semibold">Выйти из системы</Text>
+        </Flex>
+      </Flex>
     </Box>
   )
 }
